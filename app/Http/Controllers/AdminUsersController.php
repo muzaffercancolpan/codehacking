84d133fda5b996ad8dc8d010815b6e971d4fb1ee
Can Colpan <muzaffercancolpan@gmail.com>
2018-11-12 10:25:07 -0600

    Create user form added form controls

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -30,7 +32,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');
+        $roles = Role::pluck('name','id')->all();
+
+        return view('admin.users.create', compact('roles'));
 
     }
 
@@ -40,9 +44,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        return $request->all();
 
 
 
